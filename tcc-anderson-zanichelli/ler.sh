2777c9dffffb6c4f61917bd8a84d8a482b7bef11
Anderson Zanichelli <anderzanic@gmail.com>
2016-01-27 00:39:44 -0200

    Criando bibtex

--- tcc-anderson-zanichelli/ler.sh
+++ tcc-anderson-zanichelli/ler.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-rm tcc.pdf
+rm tcc.pdf tcc.aux tcc.lof tcc.log rcc.lot tcc.lsg tcc.toc tcc.bbl tcc.blg
+echo 'arquivos removidos'
 pdflatex tcc.tex
+echo 'gerados arquivos auxiliares'
+bibtex tcc
+echo 'gerado bibtex'
+pdflatex tcc.tex
+pdflatex tcc.tex
+echo 'arquivo pronto para abrir'
 evince tcc.pdf
